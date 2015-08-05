0f480d6d61262f3d77f7449db94f1e494014f5ad
PandaJJ5286 <zhanghaijing@cnpc.com.cn>
2015-08-05 17:57:02 +0800

    add photo page
    show photo

--- scripts/global.js
+++ scripts/global.js
@@ -35,6 +35,7 @@ function addClass(element, value){
 	}
 }
 
+// Navigation
 //  highlightPage()函数，页面突出显示
 
 function highlightPage(){
@@ -59,6 +60,7 @@ function highlightPage(){
 
 addLoadEvent(highlightPage);
 
+// Home
 // 幻灯片功能实现
 
 function moveElement(elementID,final_x,final_y,interval){
@@ -108,6 +110,7 @@ function moveElement(elementID,final_x,final_y,interval){
 	elem.movement = setTimeout(repeat,interval);
 }
 
+// About
 // 创建幻灯片元素并准备相应链接
 
 function prepareSlideshow(){
@@ -195,3 +198,60 @@ function prepareInternalnav(){
 }
 
 addLoadEvent(prepareInternalnav);
+
+// photos
+
+function showPic(whichPic){
+	if(!document.getElementById("placeholder")) return true;
+	var source = whichPic.getAttribute("href");
+	var placeholder = document.getElementById("placeholder");
+	placeholder.setAttribute("src",source);
+	if(!document.getElementById("description")) return false;
+	if(whichPic.getAttribute('title')){
+		var text = whichPic.getAttribute('title');
+	}else{
+		var text="";
+	}
+	var description = document.getElementById("description");
+	if(description.firstChild.nodeType == 3){
+		description.firstChild.nodeValue = text;
+	}
+	return false;
+}
+
+function preparePlaceholder(){
+	if(!document.createElement) return false;
+	if(!document.createTextNode) return false;
+	if(!document.getElementById) return false;
+	if(!document.getElementById("imagegallery")) return false;
+
+	var placeholder = document.createElement("img");
+	placeholder.setAttribute("src","images/placeholder.gif");
+	placeholder.setAttribute("alt","my image gallery")
+	placeholder.setAttribute("id","placeholder");
+
+	var description = document.createElement("p");
+	description.setAttribute("id","description");
+	var desctext = document.createTextNode("Choose an image");
+	description.appendChild(desctext);
+	var gallery = document.getElementById("imagegallery");
+	insertAfter(description,gallery);
+	insertAfter(placeholder,description);
+}
+
+function prepareGallery(){
+	if(!document.getElementsByTagName) return false;
+	if(!document.getElementById) return false;
+	if(!document.getElementById("imagegallery")) return false;
+
+	var gallery = document.getElementById("imagegallery");
+	var links = gallery.getElementsByTagName("a");
+	for (var i=0;i<links.length;i++){
+		links[i].onclick = function(){
+			return showPic(this);
+		}
+	}
+}
+
+addLoadEvent(preparePlaceholder);
+addLoadEvent(prepareGallery);
