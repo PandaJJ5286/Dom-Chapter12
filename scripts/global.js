b36f032bd659244d406b6765de4257b156acf056
PandaJJ5286 <zhanghaijing@cnpc.com.cn>
2015-08-07 17:33:58 +0800

    add contact page

--- scripts/global.js
+++ scripts/global.js
@@ -332,3 +332,92 @@ function displayAbbreviations(){
 addLoadEvent(stripeTatles);
 addLoadEvent(highlightRows);
 addLoadEvent(displayAbbreviations);
+
+
+// Contact
+
+function focusLabels(){
+	if(!document.getElementsByTagName) return false;
+	var labels = document.getElementsByTagName("label");
+
+	for (var i = 0; i < labels.length; i++){
+		if(!labels[i].getAttribute("for")) continue;
+		labels[i].onclick = function(){
+			var id = this.getAttribute("for");
+			if(!document.getElementById(id)) return false;
+			var elem = document.getElementById(id);
+			elem.focus();
+		}
+	}
+}
+
+addLoadEvent(focusLabels);
+
+function resetFields(whichform){
+	// if(Modernizr.input.placeholder) return;
+	for(var i = 0; i < whichform.elements.length; i++){
+		var element = whichform.elements[i];
+		if(element.type == "submit") continue;
+		var check = element.placeholder || element.getAttribute('placeholder');
+		if(!check) continue;
+		
+		element.onfocus = function(){
+			var text = this.placeholder || this.getAttribute('placeholder');
+			if(this.value == text){
+				this.className = "";
+				this.value = "";
+			}
+		}
+
+		element.onblur = function(){
+			if(this.value == ""){
+				this.className = 'placeholder';
+				this.value = this.placeholder || this.getAttribute('placeholder');
+			}
+		}
+
+		element.onblur();
+	}
+}
+
+function isFilled(field){
+	if(field.value.replace(' ','').length==0) return false;
+	var placeholder = field.placeholder || field.getAttribute('placeholder');
+	return (field.value != placeholder);
+}
+
+function isEmail(field){
+	return (field.value.indexOf('@') != -1 && field.value.indexOf('.') != -1);
+}
+
+function validateForm(whichform){
+	for(var i=0;i<whichform.elements.length;i++){
+		var element = whichform.elements[i];
+		if(element.required == 'required'){
+			if(!isFilled(element)){
+				alert("Please fill in the "+element.name+" field.");
+				return false;
+			}
+		}
+		if(element.type == 'email'){
+			if(!isEmail(element)){
+				alert("The "+element.name+" field must be a valid email address.");
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+function prepareForms(){
+	for(var i=0; i<document.forms.length;i++){
+		var thisform = document.forms[i];
+		resetFields(thisForm);
+		thisform.onsubmit = function(){
+			return valideteForm(this);
+		}
+	}
+}
+
+addLoadEvent(prepareForms);
+
